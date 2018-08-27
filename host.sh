bca580c2c4b91401957eef4786a0ab887f820f0c
Calvin Henry <calvinhenry60@gmail.com>
2018-08-27 18:49:17 -0400

    Renamed file, added commit command

---
+++ host.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+target_project_directory=~/Documents/CalvinHenry.github.io
+
+if [ $# == 0 ] || [ $# -gt 1 ]
+then
+    printf "Error: Invalid Number of arguments\nUsage: GithubPagesTool <target_project_directory>"
+else 
+    echo "rm -Rf '$target_project_directory/*'"
+    rm -Rf $target_project_directory/*
+    cp $1* $target_project_directory/
+    cd $target_project_directory
+    git add *
+    git commit -m "Set hosted project to project found at $1"
+    git push
+fi
